1039028a410ef8abfd514c10fb18936f5154d1d0
SB9ITOY <cdznjq2004@gmail.com>
2023-07-11 20:53:15 +0300

    +

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
         // document.write("JS Каже привіт!")
-        console.log("ghghg")
+        // console.log("ghghg")
         // console.info("wjwjwww")
         // console.error("wdadawdawdaw")
+        var num = 13234;
+        var number = "6123";
+        console.log( num + number );
