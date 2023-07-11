b17000540371e8fffec5585f70823edc568ebcff
SB9ITOY <cdznjq2004@gmail.com>
2023-07-11 21:48:03 +0300

    ada
    sd

--- main.js
+++ main.js
@@ -2,6 +2,32 @@
         // console.log("ghghg")
         // console.info("wjwjwww")
         // console.error("wdadawdawdaw")
-        var num = 13234;
-        var number = "6123";
-        console.log( num + number );
+        
+        // Математические действия
+
+        // 1 варіант !
+        var num_1 = 10;
+        var num_2 = 5;
+
+        var res = num_1 + num_2;
+        console.log("Відповідь: " + res);
+       
+       
+        // 2 варіант !
+        var num_3 = 5;
+        num_3 += 10;
+        console.log("Відповідь: " + num_3);
+
+        
+        // 3 вариант: строки переобразовывают в цифры/числа!
+        var str_1 = Number("12");
+        var str_2 = Number("2");
+
+        var res_1 = str_1 + str_2;
+        console.log("Відповідь: " + res_1);
+
+
+        // Math!
+        console.log("Math: " + Math.PI);
+        console.log("Math: " + Math.min(7, 2, 5, 9, 4, 1, 10));
+        console.log("Math: " + Math.max(7, 2, 5, 9, 4, 1, 10));
